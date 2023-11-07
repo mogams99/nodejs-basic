f9bbfa0ead0d33e62889b00e6d7282187f4c6682
Mochammad Gamal <mogams1811@gmail.com>
2023-11-07 10:49:51 +0700

    feat: :fire: root index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// const memoryInformation = process.memoryUsage();
+// console.log(memoryInformation);
+
+// const welcome = (name) => {
+//     console.log(`Welcome ${name}`);
+// }
+
+// welcome('Gamal');
