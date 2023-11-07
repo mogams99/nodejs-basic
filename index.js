494c8ee102afc8ab9b1f3d073cd189902ea963eb
Mochammad Gamal <mogams1811@gmail.com>
2023-11-07 15:50:23 +0700

    feat: :sparkles: index js
    overwrite index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const yourName = process.argv[2]; // TODO 2
+const environment = process.env.NODE_ENV; // TODO 3
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+
+// const moment = require('moment');
+// const date = moment().format("MMM Do YY");
+// console.log(date);
+
 // const memoryInformation = process.memoryUsage();
 // console.log(memoryInformation);
 
 // const welcome = (name) => {
 //     console.log(`Welcome ${name}`);
 // }
-
 // welcome('Gamal');
